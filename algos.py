a3bfc57d77cc1011c4f23394f9d7de57b1cd31c8
Nishan Singh Mann <nishan.singh.mann@gmail.com>
2016-07-18 20:47:30 -0400

    FINALLY, the index of coincidence working! Find lots of variance.
    - [x] take subsets from the ml-100k dataset and compute correlation matrix
      - [x] to take subset of the movie database, also need to take appropraite
          subset of genre_link database
    - [x] raise AssertionError if correlation matrix has nonsensical entries
    - [x] compute index of coincidence for a given number of users
    - [x] FIX a lot of bugs when computing index of coincidence
      - [x] avoid changing df_genre_corrs INPLACE. create a copy.
     - [x] avoid sampling for randn, just use genre_corrs_matrices
     - [x] generate various figures including from [Action, Adventure] to
        [Action, Adventure, Comedy, Drama] etc.

--- algos.py
+++ algos.py
@@ -18,6 +18,7 @@ def gen_corr_matrix(df_kodi_movie, df_kodi_genre, df_kodi_genre_link):
     ----------
     df_kodi_movie: Pandas dataframe with info about movie
     df_kodi_genre: dataframe with info about genres
+    df_kodi_genre_link : link between genre and idMovie
 
     Returns
     -------
@@ -76,7 +77,10 @@ def gen_corr_matrix(df_kodi_movie, df_kodi_genre, df_kodi_genre_link):
     # Normalize the total movie count for genre gi to unity
     for gi in genres:
         if df_genre_corrs.loc[gi, gi] != 0:
+            # test = df_genre_corrs.loc[gi, :]/df_genre_corrs.loc[gi, gi]
             df_genre_corrs.loc[gi, :] = df_genre_corrs.loc[gi, :]/df_genre_corrs.loc[gi, gi]
+            if (df_genre_corrs.loc[gi, :] > 1).any():
+                raise AssertionError('Correlation matrix has entries exceeding 1. Investigate.')
 
     return df_genre_corrs
 
@@ -104,7 +108,7 @@ def gen_recomm_pts(df_genre_corrs, preferred_genres_set, movie_genres_set, avg_m
     # genres
     prefactor = avg_movie_rating/len(preferred_genres_set)
     recomm_pts = 0
-    # common genres between user preffered genres and movie genre set
+    # common genres between user preferred genres and movie genre set
     gi_common = preferred_genres_set & movie_genres_set
     # if len(gi_common) != 0:
     # loop over comon genres
@@ -125,3 +129,144 @@ def gen_recomm_pts(df_genre_corrs, preferred_genres_set, movie_genres_set, avg_m
             recomm_pts += prefactor/len(movie_genres_set)*df_genre_corrs.loc[gi, gj]
 
     return recomm_pts
+
+
+# determine index of coincidence for preferred_genres_set={gi, gj} for all i,j
+def index_of_coincidence(num_users, df_movie, df_genre_corrs, genre_corrs_matrices, genres):
+    """
+    Generate average index of coincidence for the top 3 predictions.
+    This tests how stable the predictions are w.r.t. perturbations to the genre correaltion
+    matrix
+
+    Parameters
+    ----------
+    num_users : number of users
+    df_genre_corrs : Genre correlation matrix for whole dataset
+    df_movie :
+    genre_corrs_matrices : Correlation matrices generated for each user's collection
+    NOTE: num_users <= N.
+    genres : list of genres that determines the couples [gi, gj] for which
+    the index of coincidence is calculated
+
+
+    Returns
+    -------
+    avg_IOC : average index of coincidence for the top 3 recommendations
+    genre_couples_R1_ioc : index of coincidence for the top (1st) recommendation
+                           for each genre couple possible from the list of
+                           genres.
+    genre_couples_R2_ioc : index of coincidence for the second recommendation
+    genre_couples_R3_ioc : index of coincidence for the third recommendation
+    """
+
+    recomm_movies_var = pd.DataFrame(columns=['PreferredGenresSet', 'MovieName_R1', 'R1Points', 'MovieName_R2', 'R2Points', 'MovieName_R3', 'R3Points'])
+    num_genres = len(genres)
+    total_count = -1
+
+    # genres = df_genre.loc[:, 'Genres'].sort_values().tolist()
+    genres.sort()
+    # To avoid changing the dataframe df_genre_corrs in-place
+    df_genre_corrs_user = df_genre_corrs.copy()
+
+    for user_i in range(0, num_users):
+        print('\nUser number = {0}'.format(user_i))
+        # use the randomly generated corr matrix for each user
+        df_genre_corrs_user.iloc[:, :] = genre_corrs_matrices[:, :, user_i]
+        # selecting one on the diagonals didn't make much difference
+        # tmp_corrs_matrix = genre_corrs_matrices[:, :, user_i]
+        # tmp_corrs_matrix[np.diag_indices_from(tmp_corrs_matrix)] = 1
+        # df_genre_corrs_user.iloc[:, :] = tmp_corrs_matrix
+        # loop through all possible {gi, gj} couples to determine top 3 recommendations
+        for gi in genres:
+        # for gi in ['Action', 'Adventure', 'Animation', 'Children\'s']:
+            genres_geq_gi = [gj for gj in genres if gj >= gi]
+            # genres_geq_gi = [gj for gj in ['Action', 'Adventure', 'Animation', 'Children\'s'] if gj >= gi]
+            for gj in genres_geq_gi:
+                # count_genre_sets += 1
+                total_count += 1
+                preferred_genres_set = set([gi, gj])
+                recomm_movies_var.loc[total_count, 'PreferredGenresSet'] = preferred_genres_set
+                # print('Total count = {0}, Genre tuple = {1}'.format(total_count, preferred_genres_set))
+                # number of elements in upper triangular is N(N+1)/2
+                print('Total count = {0} of {1}. Genre couple = {2}'.format(total_count, num_users*0.5*num_genres*(num_genres + 1) - 1, preferred_genres_set))
+
+                # loop over movies
+                for k in df_movie.index:
+                    df_movie.loc[k, 'Recommendation Points'] = gen_recomm_pts(df_genre_corrs_user, preferred_genres_set, df_movie.loc[k, 'Genres'], float(df_movie.loc[k, 'Rating']))
+                # Normalize recommendation points column
+                df_movie.loc[:, 'Recommendation Points'] = df_movie.loc[:, 'Recommendation Points']/df_movie.loc[:, 'Recommendation Points'].max()
+                # select top 3 recommendations
+                top_3_recomms = df_movie[['MovieName', 'Recommendation Points', 'Genres', 'Rating', 'Year']].sort_values(by=['Recommendation Points'], ascending=False).head(3)
+
+                # add entries to dataframe indexed by total_count
+                recomm_movies_var.loc[total_count, 'MovieName_R1'] = top_3_recomms.loc[top_3_recomms.index[0], 'MovieName']
+                recomm_movies_var.loc[total_count, 'R1Points'] = top_3_recomms.loc[top_3_recomms.index[0], 'Recommendation Points']
+                recomm_movies_var.loc[total_count, 'MovieName_R2'] = top_3_recomms.loc[top_3_recomms.index[1], 'MovieName']
+                recomm_movies_var.loc[total_count, 'R2Points'] = top_3_recomms.loc[top_3_recomms.index[1], 'Recommendation Points']
+                recomm_movies_var.loc[total_count, 'MovieName_R3'] = top_3_recomms.loc[top_3_recomms.index[2], 'MovieName']
+                recomm_movies_var.loc[total_count, 'R3Points'] = top_3_recomms.loc[top_3_recomms.index[2], 'Recommendation Points']
+                # recomm_genre_corrs_variance.loc[total_count, '']
+
+
+    i = -1
+    j = -1
+    genre_couples_ioc = np.zeros((num_genres, num_genres), dtype='float')
+    genre_couples_R1_ioc = np.zeros((num_genres, num_genres), dtype='float')
+    genre_couples_R2_ioc = np.zeros((num_genres, num_genres), dtype='float')
+    genre_couples_R3_ioc = np.zeros((num_genres, num_genres), dtype='float')
+
+    # for gi in ['Action', 'Adventure', 'Animation', 'Children\'s']:
+    for gi in genres:
+        i += 1
+        j = i
+        # genres_geq_gi = [gj for gj in ['Action', 'Adventure', 'Animation', 'Children\'s'] if gj >= gi]
+        genres_geq_gi = [gj for gj in genres if gj >= gi]
+        for gj in genres_geq_gi:
+            # print('(i,j)=({0},{1})'.format(i, j))
+            preferred_genres_set = set([gi, gj])
+            # filter movies accordinig to genre_couple {gi, gj}
+            recomm_movies_genre_set = recomm_movies_var[recomm_movies_var.loc[:, 'PreferredGenresSet'] == preferred_genres_set]
+            movies_R1 = recomm_movies_genre_set.loc[:, 'MovieName_R1']
+            movies_R2 = recomm_movies_genre_set.loc[:, 'MovieName_R2']
+            movies_R3 = recomm_movies_genre_set.loc[:, 'MovieName_R3']
+            R1_points_mean = recomm_movies_genre_set.loc[:, ['R1Points']].mean()
+            R2_points_mean = recomm_movies_genre_set.loc[:, ['R2Points']].mean()
+            R3_points_mean = recomm_movies_genre_set.loc[:, ['R3Points']].mean()
+
+            # ioc = index of coincidence
+            # value_counts counts the number of times a unique entry occurs
+            R1_ioc = movies_R1.value_counts(normalize=True).max()
+            R2_ioc = movies_R2.value_counts(normalize=True).max()
+            R3_ioc = movies_R3.value_counts(normalize=True).max()
+            # avg_ioc = np.mean(np.array([R1_ioc, R2_ioc, R3_ioc]))
+            # compute average index of coincidence by weighting using recommendation
+            # points
+            avg_ioc = np.average(np.array([R1_ioc, R2_ioc, R3_ioc]), weights = np.squeeze(np.array([R1_points_mean, R2_points_mean, R3_points_mean])/np.sum([R1_points_mean, R2_points_mean, R3_points_mean])))
+            print('Genre Set={0}. R1 IOC = {1}. Average IOC = {2}'.format(preferred_genres_set, R1_ioc, avg_ioc))
+            # print('Genre Set={0}. Average IOC = {1}'.format(preferred_genres_set, avg_ioc))
+            genre_couples_ioc[i, j] = avg_ioc
+            genre_couples_ioc[j, i] = avg_ioc
+            genre_couples_R1_ioc[i, j] = R1_ioc
+            genre_couples_R2_ioc[i, j] = R2_ioc
+            genre_couples_R3_ioc[i, j] = R3_ioc
+            genre_couples_R1_ioc[j, i] = genre_couples_R1_ioc[i, j]
+            genre_couples_R2_ioc[j, i] = genre_couples_R2_ioc[i, j]
+            genre_couples_R3_ioc[j, i] = genre_couples_R3_ioc[i, j]
+            j += 1
+
+    return (genre_couples_ioc, genre_couples_R1_ioc, genre_couples_R2_ioc, genre_couples_R3_ioc, recomm_movies_var)
+    # perturb correlation matrix using normal dist. for each user
+    # for i in range(0, df_genre_corrs_copy.index.size):
+    #     for j in range(0, df_genre_corrs_copy.index.size):
+    #         # ASSUME a normal distribuition. You need to justify later
+    #         # perturb only off diagonal elements for now. because
+    #         # the genre should always be perfectly correlated to
+    #         # itself
+    #         if i != j:
+    #             # THIS WILL CHANGE THE DATAFRAME IN-PLACE. MUTABLE OBJECT
+    #             # df_genre_corrs.iloc[i, j] = df_genre_corrs.iloc[i, j] + genre_corrs_std_matrix[i, j]*np.random.randn(1)
+    #             # df_genre_corrs_copy.iloc[i, j] = df_genre_corrs_copy.iloc[i, j] + genre_corrs_std_matrix[i, j]*np.random.randn(1)
+    #             df_genre_corrs_copy.iloc[i, j] = df_genre_corrs_copy.iloc[i, j]
+    #             # this is needed to make sure one doesn't end up with negative correlations which will end up subtracting points
+    #             if df_genre_corrs_copy.iloc[i, j] < 0:
+    #                 df_genre_corrs_copy.iloc[i, j] = 0
--- moviereco.py
+++ moviereco.py
@@ -3,6 +3,7 @@
 Author: Nishan Singh Mann (nishan.singh.mann@gmail.com)
 
 Main setup file that reads in from the SQlite database and runs the algorithm.
+Runs statistics mimicking 100 or so different users with their collections
 """
 
 import sqlite3
@@ -13,8 +14,8 @@ import matplotlib.pyplot as plt
 from time import time
 from datetime import timedelta
 import sys
-from algos import gen_corr_matrix, gen_recomm_pts
-from parsers import parse_kodi_database, parse_movielens_database
+from algos import gen_corr_matrix, gen_recomm_pts, index_of_coincidence
+from parsers import parse_kodi_database, parse_movielens_database_100k
 
 # ----------------------------------------------------------------------- #
 # STEP 1: Parse sql data into Python
@@ -28,19 +29,55 @@ from parsers import parse_kodi_database, parse_movielens_database
 # (df_movie, df_genre, df_genre_link) = parse_kodi_database(db_path)
 
 # for movielens
-try:
-    item_path = sys.argv[1]
-    genre_path = sys.argv[2]
-    data_path = sys.argv[3]
-except:
-    print('Please specify item, genre, data paths. Exiting')
-    sys.exit()
-(df_movie, df_genre, df_genre_link) = parse_movielens_database(item_path, genre_path, data_path)
-# sys.exit()
+# try:
+#     item_path = sys.argv[1]
+#     genre_path = sys.argv[2]
+#     data_path = sys.argv[3]
+# except:
+#     print('Please specify item, genre, data paths. Exiting')
+#     sys.exit()
+item_path = './MovieLens/ml-100k/u.item'
+genre_path = './MovieLens/ml-100k/u.genre'
+data_path = './MovieLens/ml-100k/u.data'
+(df_movie, df_genre, df_genre_link) = parse_movielens_database_100k(item_path, genre_path, data_path)
+
+
 
 # ----------------------------------------------------------------------- #
 # STEP 2, Compute genre correlations
 # create genre correlations dataframe
+num_users = 100
+num_movies_per_user = 150
+num_genres = df_genre.index.size
+sample_users = np.random.random_integers(df_movie.index.min(), df_movie.index.max(), size=(num_users, num_movies_per_user))
+
+# ENSURE the movie ids generated for each user are unique.
+user_i = 0
+while user_i < num_users:
+    unique_sample_movies = 0
+    while unique_sample_movies < num_movies_per_user:
+        sample_movies = np.random.random_integers(df_movie.index.min(), df_movie.index.max(), size=(1, num_movies_per_user))
+        unique_sample_movies = np.unique(sample_movies).size
+    sample_users[user_i, :] = sample_movies
+    print('{0} unique movies set for user number {1} generated.'.format(num_movies_per_user, user_i))
+    user_i += 1
+
+genre_corrs_matrices = np.zeros((num_genres, num_genres, num_users), dtype='float')
+genre_corrs_mean_matrix = np.zeros((num_genres, num_genres), dtype='float')
+genre_corrs_std_matrix = np.zeros((num_genres, num_genres), dtype='float')
+
+for user_i in range(0, num_users):
+    print('User number={0}'.format(user_i))
+    df_movie_subset = df_movie.loc[sample_users[user_i, :]]
+    df_genre_link_subset = df_genre_link[df_genre_link.loc[:, 'idMovie'].isin(df_movie_subset.index)]
+    df_genre_corrs = gen_corr_matrix(df_movie_subset, df_genre, df_genre_link_subset)
+    genre_corrs_matrices[:, :, user_i] = np.array(df_genre_corrs)
+
+for i in range(0, num_genres):
+    for j in range(0, num_genres):
+        genre_corrs_mean_matrix[i, j] = np.mean(genre_corrs_matrices[i, j, :])
+        genre_corrs_std_matrix[i, j] = np.std(genre_corrs_matrices[i, j, :])
+
 start = time()
 df_genre_corrs = gen_corr_matrix(df_movie, df_genre, df_genre_link)
 end = time()
@@ -48,51 +85,109 @@ elapsed_time = timedelta(seconds=end-start)
 print('Genre correlations matrix created. {0}'.format(elapsed_time))
 
 # Visualize genre correlations
-plt.figure()
-plt.pcolormesh(np.array(df_genre_corrs, dtype=float), cmap='gnuplot', edgecolor=None)
-plt.colorbar()
+fig = plt.figure()
+fig.add_axes([0.1,0.1,0.85,0.85])
+
 # create labels
 x_points = []
 x_labels = []
 for i in range(0, df_genre_corrs.index.size):
     x_points.append(i + 0.5)
     x_labels.append('{0:.3}'.format(df_genre_corrs.index[i]))
+plt.pcolormesh(genre_corrs_mean_matrix, cmap='gnuplot', edgecolor=None, vmin=0, vmax=1)
+plt.xticks(x_points, x_labels)
+plt.yticks(x_points, x_labels)
+plt.colorbar(orientation='horizontal')
+plt.xlim(0, df_genre_corrs.index.size)
+plt.ylim(0, df_genre_corrs.index.size)
+plt.title('Mean genre correlation matrix for {0} users'.format(num_users))
+plt.savefig('mean_genre_corrs_matrix.pdf')
+
+fig = plt.figure()
+fig.add_axes([0.1,0.1,0.85,0.85])
+plt.pcolormesh(genre_corrs_std_matrix, cmap='gnuplot', edgecolor=None, vmin=0, vmax=1)
+plt.xticks(x_points, x_labels)
+plt.yticks(x_points, x_labels)
+plt.colorbar(orientation='horizontal')
+plt.xlim(0, df_genre_corrs.index.size)
+plt.ylim(0, df_genre_corrs.index.size)
+plt.title('Std genre correlation matrix for {0} users'.format(num_users))
+plt.savefig('std_genre_corrs_matrix.pdf')
+
+fig = plt.figure()
+fig.add_axes([0.1,0.1,0.85,0.85])
+plt.pcolormesh(np.array(df_genre_corrs, dtype=float), cmap='gnuplot', edgecolor=None, vmin=0, vmax=1)
+plt.colorbar(orientation='horizontal')
 plt.xticks(x_points, x_labels)
 plt.yticks(x_points, x_labels)
 plt.xlim(0, df_genre_corrs.index.size)
 plt.ylim(0, df_genre_corrs.index.size)
-plt.title('Genre Correlations Matrix')
+plt.title('Genre correlations matrix for {0} movies'.format(df_movie.index.size))
+plt.savefig('full_genre_corrs_matrix.pdf')
+
 plt.show()
 
 # ----------------------------------------------------------------------- #
 
 # ----------------------------------------------------------------------- #
 # STEPS 3 and 4. Specify user preferred genres and compute recommendation
+
+# genres = ['Action', 'Adventure', 'Comedy', 'Drama', 'Romance', 'Horror', 'Thriller']
+genres = ['Action', 'Adventure', 'Comedy', 'Drama', 'Romance', 'Horror', 'Thriller']
+num_users_subset = num_users
+(genre_couples_ioc, genre_couples_R1_ioc, genre_couples_R2_ioc, genre_couples_R3_ioc, recomm_movies_var) = index_of_coincidence(num_users_subset, df_movie, df_genre_corrs, genre_corrs_matrices, genres)
+
+ # recomm_movies_var[recomm_movies_var.loc[:, 'PreferredGenresSet'] == set(['Action'])].loc[:, ['MovieName_R1', 'R1Points']]
+
+# create labels
+x_points = []
+x_labels = []
+for i in range(0, len(genres)):
+    x_points.append(i + 0.5)
+    x_labels.append('{0:.3}'.format(genres[i]))
+
+fig = plt.figure()
+fig.add_axes([0.1, 0.1, 0.85, 0.85])
+plt.pcolormesh(genre_couples_ioc, cmap='gnuplot', edgecolor=None, vmin=0, vmax=1)
+plt.xticks(x_points, x_labels)
+plt.yticks(x_points, x_labels)
+plt.colorbar(orientation='horizontal')
+plt.xlim(0, len(genres))
+plt.ylim(0, len(genres))
+plt.title('Average index of coincidence for {0} users ({1} movies each)'.format(num_users_subset, num_movies_per_user))
+plt.savefig('avg_ioc_' + '_'.join(genres) + '_' + str(num_users) + '.pdf')
+
+
 # points for each movie
-prompt = ''
-while not (prompt == 'y' or prompt == 'n'):
-    prompt = str(input('Would you like to specify a genre set? If you want a list of genres, type l [y/n/l]: '))
-    if prompt == 'y':
-        genre_ids_input = input('Enter the genreIDs of interest. E.g. entering 0,1 means you are interested in {0}, {1}: '.format(df_genre.loc[0, 'Genres'], df_genre.loc[1, 'Genres']))
-        try:
-            preferred_genres_set = set([df_genre.loc[int(i), 'Genres'] for i in genre_ids_input.split(',')])
-            print('Preferred genres = {0}'.format(preferred_genres_set))
-            # loop over movies
-            for k in df_movie.index:
-                df_movie.loc[k, 'Recommendation Points'] = gen_recomm_pts(df_genre_corrs, preferred_genres_set, df_movie.loc[k, 'Genres'], float(df_movie.loc[k, 'Rating']))
-            # Normalize recommendation points column
-            df_movie.loc[:, 'Recommendation Points'] = df_movie.loc[:, 'Recommendation Points']/df_movie.loc[:, 'Recommendation Points'].max()            
-            print('Here are your top 10 recommendations.')
-            print(df_movie[['MovieName', 'Recommendation Points', 'Genres', 'Rating', 'Year']].sort_values(by=['Recommendation Points'], ascending=False).head(10))
-            prompt = ''
-        except:
-            print('Could not read genres, please specify correctly')
-            prompt = ''
-    elif prompt == 'l':
-        print('Here is the list of genres. Left Column: genreID, Right Column: Genre\n{0}'.format(df_genre.Genres.sort_values()))
-    elif prompt == 'n':
-        print('Thanks for trying out the algorithm. Feedback welcome.')
-        sys.exit()
+# prompt = ''
+# while not (prompt == 'y' or prompt == 'n'):
+#     prompt = str(input('Would you like to specify a genre set? If you want a list of genres, type l [y/n/l]: '))
+#     if prompt == 'y':
+#         genre_ids_input = input('Enter the genreIDs of interest. E.g. entering 0,1 means you are interested in {0}, {1}: '.format(df_genre.loc[0, 'Genres'], df_genre.loc[1, 'Genres']))
+#         try:
+#             preferred_genres_set = set([df_genre.loc[int(i), 'Genres'] for i in genre_ids_input.split(',')])
+#             print('Preferred genres = {0}'.format(preferred_genres_set))
+#             # perturb correaltion matrix
+#             for i in range(0, df_genre_corrs.index.size):
+#                 for j in range(0, df_genre_corrs.index.size):
+#                     df_genre_corrs.iloc[i, j] = df_genre_corrs.iloc[i, j] + genre_corrs_std_matrix[i, j]*np.random.randn(1)
+#             # loop over movies
+#             for k in df_movie.index:
+#                 df_movie.loc[k, 'Recommendation Points'] = gen_recomm_pts(df_genre_corrs, preferred_genres_set, df_movie.loc[k, 'Genres'], float(df_movie.loc[k, 'Rating']))
+#             # Normalize recommendation points column
+#             df_movie.loc[:, 'Recommendation Points'] = df_movie.loc[:, 'Recommendation Points']/df_movie.loc[:, 'Recommendation Points'].max()
+#             print('Here are your top 10 recommendations.')
+#             print(df_movie[['MovieName', 'Recommendation Points', 'Genres', 'Rating', 'Year']].sort_values(by=['Recommendation Points'], ascending=False).head(10))
+#             prompt = ''
+#         except:
+#             print('Could not read genres, please specify correctly')
+#             prompt = ''
+#     elif prompt == 'l':
+#         print('Here is the list of genres. Left Column: genreID, Right Column: Genre\n{0}'.format(df_genre.Genres.sort_values()))
+#     elif prompt == 'n':
+#         print('Thanks for trying out the algorithm. Feedback welcome.')
+#         sys.exit()
+
 
 # preferred_genres_set = {'Drama', 'Comedy'}
 #
